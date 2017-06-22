3a75290f2466641f4ac12591156b66e3fe6ab4a7
Slawomir Polanski <polanski.slawomir@gmail.com>
2017-06-22 22:58:24 +0000

    rebuild pages at 9f8b9ac

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+  exit 0
+fi
+
+rev=$(git rev-parse --short HEAD)
+
+cd _site
+
+git init
+git config user.name "Slawomir Polanski"
+git config user.email "polanski.slawomir@gmail.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/spolanski/blogTest.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+#echo "spolanski.github.io/test_1/" > CNAME
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
+
+
--- menu/contact.md
+++
@@ -1,6 +0,0 @@
----
-layout: page
-title: Contact
----
-
-If you are having any questions/suggestions or you would like to talk, feel free to write to me on [polanski.slawomir@gmail.com](polanski.slawomir@gmail.com). You can also find me on [LinkedIn](https://linkedin.com/in/slawomirpolanski) and [GitHub](https://github.com/spolanski).
